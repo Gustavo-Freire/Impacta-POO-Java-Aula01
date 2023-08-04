9ad4f2c60d8bbfcdd82d8f5f444e4cd3660d6c02
Freire <gustavoarthur.freire@gmail.com>
2023-08-04 18:52:34 -0300

    Organizando loop execucao

--- src/App.java
+++ src/App.java
@@ -3,17 +3,36 @@ import java.util.*;
 public class App {
     public static void main(String[] args){
         Scanner escolha = new Scanner(System.in);
-        System.out.println("Deseja jogar?(S/N)");
-        String continuar = escolha.nextLine();  // Read user input
-        escolha.close();
-        System.out.println(continuar);
-        if (continuar.equals("S")){
-            System.out.println("Jogando");
-        }else if (continuar.equals("N")){
-            System.out.println("Parando de Jogar");
-        }else{
-            System.out.println("resposta invalida");
+        while (true) {
+        
+            System.out.println("Escolha uma opção:");
+
+            System.out.println("0 - Sair");
+            System.out.println("1 - Pedra");
+            System.out.println("2 - Papel");
+            System.out.println("3 - Tesoura");
+
+            int continuar = escolha.nextInt();  
+            
+            System.out.println(continuar);
+
+            if (continuar == 0){
+                System.out.println("Parando de Jogar");
+                break;
+            } else if (continuar == 1){
+                System.out.println("Escolheu Pedra");
+                break;
+            } else if (continuar == 2){
+                System.out.println("Escolheu Papel");
+                break;
+            } else if (continuar == 3){
+                System.out.println("Escolheu Tesoura");
+                break;
+            } else {
+                System.out.println("resposta invalida");
+            }   
         }
+        escolha.close();
     }
 }
 
