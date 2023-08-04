0f71a129cd572de803b410ec489cee88ef24f58a
Gustavo Arthur Freire <2200256@SALAS.AULAS>
2023-08-03 22:05:41 -0300

    funcionou

--- src/App.java
+++ src/App.java
@@ -1,7 +1,5 @@
 import java.util.*;
 
-
-
 public class App {
     public static void main(String[] args){
         Scanner escolha = new Scanner(System.in);
@@ -9,9 +7,9 @@ public class App {
         String continuar = escolha.nextLine();  // Read user input
         escolha.close();
         System.out.println(continuar);
-        if (continuar == "S"){
+        if (continuar.equals("S")){
             System.out.println("Jogando");
-        }else if (continuar == "N"){
+        }else if (continuar.equals("N")){
             System.out.println("Parando de Jogar");
         }else{
             System.out.println("resposta invalida");
