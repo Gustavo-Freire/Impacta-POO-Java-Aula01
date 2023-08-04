10fa6fa2c2d964fe5b8cdf8e55ca69480a7fe6ee
Gustavo Arthur Freire <2200256@SALAS.AULAS>
2023-08-03 22:03:42 -0300

    teste

--- src/App.java
+++ src/App.java
@@ -1,5 +1,21 @@
+import java.util.*;
+
+
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!!!");
+    public static void main(String[] args){
+        Scanner escolha = new Scanner(System.in);
+        System.out.println("Deseja jogar?(S/N)");
+        String continuar = escolha.nextLine();  // Read user input
+        escolha.close();
+        System.out.println(continuar);
+        if (continuar == "S"){
+            System.out.println("Jogando");
+        }else if (continuar == "N"){
+            System.out.println("Parando de Jogar");
+        }else{
+            System.out.println("resposta invalida");
+        }
     }
 }
+
